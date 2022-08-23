86737cbacf030ae67d244d8a08a2a22ef4e1de34
07.Mayank-raj <2003.mayank.raj@gmail.com>
2022-08-23 16:37:54 +0530

    Queues opration enqueue,dueue,peek

--- Stack and queues/Queues.js
+++ Stack and queues/Queues.js
@@ -7,23 +7,52 @@ class Node{
 
  class Queue {
     constructor(){
-        this.top = null;
-        this.bottom =null;
+        this.first = null;
+        this.last  =null;
         this.length = 0;
     }
 
     peek(){
         // return the peek element of stack
+        return this.first;
     }
     enqueue(value){
         //code
+
+        const newNode = new Node(value);
+       
+        if(this.length === 0){                    //if stack is empty 
+            this.first = newNode;                //top and bottom will follow same node
+            this.last = newNode;
+        }
+        else{
+             this.last.next = newNode;
+             this.last  = newNode;
+        }
+        this.length++;
+        return this;
     }
     dequeue(){
         //code
+        if(!this.first){
+            return null;
+        }
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.length--;
     }
      
  }
 //_________________________________________________________
  const myQueue = new Queue();
+ myQueue.enqueue('what');
+ myQueue.enqueue('the');
+ myQueue.enqueue('fuck');
  console.log(myQueue);
+ console.log(myQueue.peek());
+ myQueue.dequeue();
+ console.log(myQueue);
+ 
  
